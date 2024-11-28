c40e750e7ac13183e266e29c981ca724fdf26034
Dario12311 <111399824+Dario12311@users.noreply.github.com>
2024-11-28 03:49:33 -0500

    Origin/features proyecto notas dario (#3)
    * [COMMIT: PBI_Entity,DAL,BLL] Creacion de Clases
    
    * [COMMIT: PBI_ALUMNO] enlace del FRM_Alumno
    
    * [COMMIT:PBI_SALON] enlace Salon
    
    * Ok
    
    * ok
    
    * Se ajusta
    
    ---------
    
    Co-authored-by: William Yaruro <william.yaruro@suncapital.es>

--- DAL/GestionSalon/SalonDAL.cs
+++ DAL/GestionSalon/SalonDAL.cs
@@ -15,15 +15,14 @@ namespace DAL
             using (var connection = new OracleConnection(OracleConnectionString.CadenaConexion))
             {
                 await connection.OpenAsync();
-                var command = new OracleCommand("pq_Salon.pr_Insertar", connection)
+                using (var command = new OracleCommand("pq_Salon.pr_Insertar", connection))
                 {
-                    CommandType = CommandType.StoredProcedure
-                };
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("p_NombreSalon", OracleDbType.Varchar2).Value = salon.NombreSalon;
+                    command.Parameters.Add("p_GradoId_FK", OracleDbType.Int32).Value = salon.GradoId_FK;
 
-                command.Parameters.Add("p_NombreSalon", OracleDbType.Varchar2).Value = salon.NombreSalon;
-                command.Parameters.Add("p_GradoId_FK", OracleDbType.Int32).Value = salon.GradoId_FK;
-
-                await command.ExecuteNonQueryAsync();
+                    await command.ExecuteNonQueryAsync();
+                }
             }
         }
 
@@ -33,16 +32,15 @@ namespace DAL
             using (var connection = new OracleConnection(OracleConnectionString.CadenaConexion))
             {
                 await connection.OpenAsync();
-                var command = new OracleCommand("pq_Salon.pr_Actualizar", connection)
+                using (var command = new OracleCommand("pq_Salon.pr_Actualizar", connection))
                 {
-                    CommandType = CommandType.StoredProcedure
-                };
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("p_SalonId", OracleDbType.Int32).Value = salon.SalonId;
+                    command.Parameters.Add("p_NombreSalon", OracleDbType.Varchar2).Value = salon.NombreSalon;
+                    command.Parameters.Add("p_GradoId_FK", OracleDbType.Int32).Value = salon.GradoId_FK;
 
-                command.Parameters.Add("p_SalonId", OracleDbType.Int32).Value = salon.SalonId;
-                command.Parameters.Add("p_NombreSalon", OracleDbType.Varchar2).Value = salon.NombreSalon;
-                command.Parameters.Add("p_GradoId_FK", OracleDbType.Int32).Value = salon.GradoId_FK;
-
-                await command.ExecuteNonQueryAsync();
+                    await command.ExecuteNonQueryAsync();
+                }
             }
         }
 
@@ -54,24 +52,23 @@ namespace DAL
             using (var connection = new OracleConnection(OracleConnectionString.CadenaConexion))
             {
                 await connection.OpenAsync();
-                var command = new OracleCommand("pq_Salon.pr_TraerPorID", connection)
+                using (var command = new OracleCommand("pq_Salon.pr_TraerPorID", connection))
                 {
-                    CommandType = CommandType.StoredProcedure
-                };
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("p_SalonId", OracleDbType.Int32).Value = salonId;
+                    command.Parameters.Add("p_Resultado", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
 
-                command.Parameters.Add("p_SalonId", OracleDbType.Int32).Value = salonId;
-                command.Parameters.Add("p_Resultado", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
-
-                using (var reader = await command.ExecuteReaderAsync())
-                {
-                    if (await reader.ReadAsync())
+                    using (var reader = await command.ExecuteReaderAsync())
                     {
-                        salon = new Salon
+                        if (await reader.ReadAsync())
                         {
-                            SalonId = reader.GetInt32(0),
-                            NombreSalon = reader.GetString(1),
-                            GradoId_FK = reader.GetInt32(2)
-                        };
+                            salon = new Salon
+                            {
+                                SalonId = reader.GetInt32(0),
+                                NombreSalon = reader.GetString(1),
+                                GradoId_FK = reader.GetInt32(2)
+                            };
+                        }
                     }
                 }
             }
@@ -87,23 +84,22 @@ namespace DAL
             using (var connection = new OracleConnection(OracleConnectionString.CadenaConexion))
             {
                 await connection.OpenAsync();
-                var command = new OracleCommand("pq_Salon.pr_TraerTodos", connection)
+                using (var command = new OracleCommand("pq_Salon.pr_TraerTodos", connection))
                 {
-                    CommandType = CommandType.StoredProcedure
-                };
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("p_Resultado", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
 
-                command.Parameters.Add("p_Resultado", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
-
-                using (var reader = await command.ExecuteReaderAsync())
-                {
-                    while (await reader.ReadAsync())
+                    using (var reader = await command.ExecuteReaderAsync())
                     {
-                        salones.Add(new Salon
+                        while (await reader.ReadAsync())
                         {
-                            SalonId = reader.GetInt32(0),
-                            NombreSalon = reader.GetString(1),
-                            GradoId_FK = reader.GetInt32(2)
-                        });
+                            salones.Add(new Salon
+                            {
+                                SalonId = reader.GetInt32(0),
+                                NombreSalon = reader.GetString(1),
+                                GradoId_FK = reader.GetInt32(2)
+                            });
+                        }
                     }
                 }
             }
